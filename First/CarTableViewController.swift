af49a3e6e35de5f665693d02bb60359a6e5d80fd
Koval1997 <kovalmax06@gmail.com>
2018-06-01 03:42:57 -0700

    Add files via upload

--- First/CarTableViewController.swift
+++ First/CarTableViewController.swift
@@ -7,8 +7,8 @@
 //
 
 import UIKit
-
-struct Car{
+import Alamofire
+struct Car : Codable{
     let carName : String?
     let carPrice : String?
     let carImage : String?
@@ -18,27 +18,23 @@ struct Car{
     let mileage : String?
     let salerName : String?
     
-    static func parse(json: Dictionary<String, Any>) -> Car? {
-        guard let autoName = json["auto"] as? String, let price = json["Price"] as? String, let saler_phone = json["Phone"] as? String,
-        let descr = json["Descr"] as? String,  let mile = json["Mileage"] as? String, let saler_name = json["name"] as? String else {
-            return nil
-        }
-        
-        return Car(carName: autoName, carPrice: price, carImage: autoName, carYear: "2015", salerPhone: saler_phone, description: descr, mileage: mile, salerName: saler_name)
-    }
 }
 
 
 class CarTableViewController: UITableViewController {
 
     @IBOutlet var tblCars: UITableView!
-    public var cars: [Car] = []
+    public var cars : [Car] = []
     var row = 0;
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tblCars.delegate = self
-        load()
+        loadData()
+        if (cars.count == 0)
+        {
+            setup()
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,17 +44,30 @@ class CarTableViewController: UITableViewController {
     
     public func setup() {
         cars = [
-            Car(carName: "Nissan Teana", carPrice: "1100000", carImage: "Nissan Teana", carYear: "2015", salerPhone: "88003553535", description: "Куплен в 2013г. Крашенных элементов нет, бесключевой доступ, запуск с кнопки, цветной дисплей, Сигнализация с автозапуском, 2 комплекта резины. Я 2-й хозяин, автомобиль беспроблемный очень надежный. Поменяны все жидкости, фильтра, колодки. Автосалоны не звоните.", mileage: "18900", salerName:"NIKOLAI" ),
+            Car(carName: "Nissan Teana", carPrice: "1100000", carImage: "Nissan Teana", carYear: "2012", salerPhone: "88003553535", description: "Куплен в 2013г. Крашенных элементов нет, бесключевой доступ, запуск с кнопки, цветной дисплей, Сигнализация с автозапуском, 2 комплекта резины. Я 2-й хозяин, автомобиль беспроблемный очень надежный. Поменяны все жидкости, фильтра, колодки. Автосалоны не звоните.", mileage: "18900", salerName:"NIKOLAI" ),
             Car(carName: "Toyota Camry", carPrice: "1250000", carImage: "Toyota Camry", carYear: "2016", salerPhone: "88003553535", description: "Хороший японский автомобиль, надёжный и очень резвый. Пробег реальный, обслуживалась вовремя. Есть косяки по кузову. Резина ёкохама - зима, новая. Все вопросы при осмотре.", mileage: "12400", salerName:"PETR" ),
             Car(carName: "Mazda 6", carPrice: "1400000", carImage: "Mazda 6", carYear: "2017", salerPhone: "88003553535", description: "Продам мазду 6 в отличном техническом и внешнем состоянии! Все работает отлично! Документы в порядке!", mileage: "15000", salerName:"FEDOR")
         ]
+        tableView.reloadData()
     }
     
-    public func load() {
-        JsonRequest().getCars(){ cars in
-            self.cars = cars
-            self.tableView.reloadData()
+    
+    public func loadData() {
+        let url = URL(string: "http://api.kovalmax06.netobject.sandbox/site/get")
+        
+        Alamofire.request(url!).responseJSON { (response) in
+            
+            let result = response.data
+            do
+            {
+                self.cars = try JSONDecoder().decode([Car].self, from: result!)
+                print("OK")
+            } catch
+            {
+                print("error")
+            }
         }
+        
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
